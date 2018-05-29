8f397b444a51def980b12b21b3bed1a8bb2ec833
Laura Oliveira <laura.regina.cp@gmail.com>
2018-05-29 20:37:33 -0300

    Criação da estrutura geral da aplicação

---
+++ app/src/main/java/acheiaqui/com/acheiaqui/ProfileShopActivity.java
@@ -0,0 +1,29 @@
+package acheiaqui.com.acheiaqui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class ProfileShopActivity extends AppCompatActivity
+{
+
+    private static final String[] nameShop = {"Catatau", "Baraca da Tia", "Hamgurgao do Carlos"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_shop);
+
+
+        ListView profileShopList = (ListView) findViewById(R.id.profile_shop_listView);
+        //porque eu preciso passar de um contexto para criar uma lista?
+       // ArrayAdapter<String> nameShopAdapter = ;
+        profileShopList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, (List<String>) profileShopList));
+
+
+    }
+}
---
+++ app/src/main/java/acheiaqui/com/acheiaqui/RegisterShopActivity.java
@@ -0,0 +1,13 @@
+package acheiaqui.com.acheiaqui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_shop);
+    }
+}

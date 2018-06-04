5421c58a7e6c9ce2ed332cef7f9f73b8b2ff0299
Laura Oliveira <laura.regina.cp@gmail.com>
2018-06-04 15:38:39 -0300

    Ajustes na tela de cadastro de estabelecimento

---
+++ app/src/main/java/acheiaqui/com/acheiaqui/ConfirmShopActivity.java
@@ -0,0 +1,22 @@
+package acheiaqui.com.acheiaqui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ConfirmShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_shop);
+
+    }
+
+    public void onClickButton(View view)
+    {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/acheiaqui/com/acheiaqui/RegisterShopActivity.java
+++ app/src/main/java/acheiaqui/com/acheiaqui/RegisterShopActivity.java
@@ -1,13 +1,25 @@
 package acheiaqui.com.acheiaqui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class RegisterShopActivity extends AppCompatActivity {
-
+public class RegisterShopActivity extends AppCompatActivity
+{
+   // Button btnRegister = (Button) findViewById(R.id.btn_register);
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_shop);
+    }
+
+  //  Button btnRegister = (Button) findViewById(R.id.btn_register);
+
+    public void onClickButton(View view)
+    {
+        Intent intent = new Intent(RegisterShopActivity.this,ConfirmShopActivity.class);
+        startActivity(intent);
     }
 }

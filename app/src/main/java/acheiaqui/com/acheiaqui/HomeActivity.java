1b3d766ff2a9a28def732892eeb444019de01042
Laura Oliveira <laura.regina.cp@gmail.com>
2018-06-11 10:35:50 -0300

    =Criação do mapa da tela Home

--- app/src/main/java/acheiaqui/com/acheiaqui/HomeActivity.java
+++ app/src/main/java/acheiaqui/com/acheiaqui/HomeActivity.java
@@ -1,39 +1,45 @@
 package acheiaqui.com.acheiaqui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity
-{
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    private static final Shop[] nameShop = {
-            /*new Shop("Catatau", "O mestre dos salgados", "Galandru"),
-            new Shop("Hamburgao do Roberto Carlos", "O Rei dos hamburguers", "Cair Paravel"),
-            new Shop("Barraquinha da Tia", "A tia que sempre tem aquele guaraná do Amazonas", "Moria")*/
-    };
+public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
 
-        ListView searchShopList = (ListView) findViewById(R.id.search_shop_listView);
-        //porque eu preciso passar de um contexto para criar uma lista?
-        searchShopList.setAdapter(new ShopArrayListAdapter(this, R.layout.shop_name_listitem, R.id.shop_name, nameShop));
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-        searchShopList.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
-            {
-                Toast.makeText(parent.getContext(), "Estabelecimento selecionado: " + nameShop[position].getName(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        // Add a marker in Sydney and move the camera
+        LatLng recife = new LatLng(-8.0538900, -34.8811100);
+        mMap.addMarker(new MarkerOptions().position(recife).title("Recife"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(recife));
     }
 }

5dffdefc606549c96d0cc4baabd865885efa212d
Laura Oliveira <laura.regina.cp@gmail.com>
2018-07-08 12:54:54 -0300

    Modificação  para implementar a tela de seleção de comidas oferecidas pelo estabelecimento

--- app/src/main/java/acheiaqui/com/acheiaqui/RegisterFoodListActivity.java
+++ app/src/main/java/acheiaqui/com/acheiaqui/RegisterFoodListActivity.java
@@ -9,30 +9,20 @@ import android.widget.Toast;
 
 public class RegisterFoodListActivity extends AppCompatActivity
 {
-
-    private static final Shop[] nameShop = {
-            /*new Shop("Catatau", "O mestre dos salgados", "Galandru"),
-            new Shop("Hamburgao do Roberto Carlos", "O Rei dos hamburguers", "Cair Paravel"),
-            new Shop("Barraquinha da Tia", "A tia que sempre tem aquele guaraná do Amazonas", "Moria")*/
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_list);
 
+        final ListView foodItemList = (ListView) findViewById(R.id.foodItem_listView);
 
-        ListView searchShopList = (ListView) findViewById(R.id.search_shop_listView);
-        //porque eu preciso passar de um contexto para criar uma lista?
-        searchShopList.setAdapter(new ShopArrayListAdapter(this, R.layout.shop_name_listitem, R.id.shop_name, nameShop));
-
-        searchShopList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        foodItemList.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
             {
-                Toast.makeText(parent.getContext(), "Estabelecimento selecionado: " + nameShop[position].getName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(parent.getContext(), "Comida selecionada!", Toast.LENGTH_LONG).show();
             }
         });
     }

536844b9fb5c9b751b9452558e4245942e50a0ea
Ramon Vicente <ramonvicentee@gmail.com>
2018-07-12 20:59:53 -0300

    Exibição de imagem do ponto de venda

--- app/src/main/java/acheiaqui/com/acheiaqui/ProfileShopActivity.java
+++ app/src/main/java/acheiaqui/com/acheiaqui/ProfileShopActivity.java
@@ -2,14 +2,15 @@ package acheiaqui.com.acheiaqui;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +21,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
@@ -38,7 +40,6 @@ public class ProfileShopActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        ImageView imageShop = (ImageView) findViewById(R.id.profile_image_shop);
         TextView nameShopText = (TextView) findViewById(R.id.profile_name_shop);
         TextView infoShopText = (TextView) findViewById(R.id.profile_info_shop);
         TextView referencePointText = (TextView) findViewById(R.id.profile_reference_point);
@@ -53,35 +54,21 @@ public class ProfileShopActivity extends AppCompatActivity {
         referencePointText.setText(id);
 
         FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-        StorageReference storageReference = firebaseStorage.getReference().child("pictures").child(id);
+        StorageReference storageReference = firebaseStorage.getReferenceFromUrl("gs://acheiaqui-3a20f.appspot.com/").child("pictures").child(id);
 
-        Glide.with(this /* context */)
-                .load(storageReference)
-                .into(imageShop);
-
-
-       /* auth = FirebaseAuth.getInstance();
-
-        final DatabaseReference referenceDatabase = FirebaseDatabase.getInstance().getReference("acheiaqui");
-        DatabaseReference shopReference = referenceDatabase.child("shop").child("ID");
-        final DatabaseReference nameShopFirebase = referenceDatabase.child("name");
-        DatabaseReference infoShopFirebase = shopReference.child("info");
-        DatabaseReference pointReferenceShopFirebase = shopReference.child("reference");
-        DatabaseReference addressShopFirebase = shopReference.child("place");
-
-        valueEventListener = new ValueEventListener() {
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                nameShopText.setText(String.valueOf(dataSnapshot.getValue(Shop.class)));
-                infoShopText.setText(String.valueOf(dataSnapshot.getValue(String.class)));
-                referencePointText.setText(String.valueOf(dataSnapshot.getValue(String.class)));
-                addressShopText.setText(String.valueOf(dataSnapshot.getValue(String.class)));
+            public void onSuccess(Uri uri) {
+
+                ImageView imageShop = (ImageView) findViewById(R.id.profile_image_shop);
+                Picasso.get().load(uri).into(imageShop);
             }
-
+        }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onFailure(@NonNull Exception e) {
 
             }
-        };*/
+        });
+
     }
 }
